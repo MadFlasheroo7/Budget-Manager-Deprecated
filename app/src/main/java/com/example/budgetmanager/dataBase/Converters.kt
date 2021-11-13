9907c536d0f8174c028550a012662d8f3d7083af
MadFlasheroo7 <jayeshseth45@gmail.com>
2021-11-13 17:07:36 +0000

    emergency push

--- app/src/main/java/com/example/budgetmanager/dataBase/Converters.kt
+++ app/src/main/java/com/example/budgetmanager/dataBase/Converters.kt
@@ -1,33 +1,4 @@
 package com.example.budgetmanager.dataBase
 
-import androidx.room.TypeConverter
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-
-class ExpensesTypeConverter {
-    @TypeConverter
-    fun fromString(value: String?): MutableList<String> {
-        val listType = object:
-            TypeToken<MutableList<String>>(){}.type
-        return Gson().fromJson(value, listType)
-    }
-
-    @TypeConverter
-    fun fromList(list: MutableList<String?>): String {
-        return Gson().toJson(list)
-    }
-}
-
-class ExpensesTitleTypeConverter {
-    @TypeConverter
-    fun fromString(value: String?): MutableList<String> {
-        val listType = object:
-            TypeToken<MutableList<String>>(){}.type
-        return Gson().fromJson(value, listType)
-    }
-
-    @TypeConverter
-    fun fromList(list: MutableList<String?>): String {
-        return Gson().toJson(list)
-    }
+class Converters {
 }
